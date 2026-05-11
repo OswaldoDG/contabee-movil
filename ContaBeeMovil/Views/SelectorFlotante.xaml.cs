95d82b27f9989c210bdb46996484bf443bcca1f4
ArturMX23 <mediaob11@gmail.com>
2026-05-11 07:51:55 -0600

    Devoluciones y Comprobaciones Rebase

--- ContaBeeMovil/Views/SelectorFlotante.xaml.cs
+++ ContaBeeMovil/Views/SelectorFlotante.xaml.cs
@@ -78,7 +78,6 @@ public partial class SelectorFlotante : ContentView
     public event EventHandler<int>? IndiceCambiado;
 
     private bool _sincronizando;
-    private bool _dropdownEmbebidoVisible;
 
     public SelectorFlotante()
     {
