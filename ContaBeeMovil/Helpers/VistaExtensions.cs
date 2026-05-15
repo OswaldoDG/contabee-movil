046cac6e42b9d2a0ca6ae1c60c512b7e3ef0af10
ArturMX23 <mediaob11@gmail.com>
2026-05-15 10:02:07 -0600

    Correccion Selector

--- ContaBeeMovil/Helpers/VistaExtensions.cs
+++ ContaBeeMovil/Helpers/VistaExtensions.cs
@@ -1,57 +1,17 @@
-using CommunityToolkit.Maui.Views;
-
 namespace ContaBeeMovil.Helpers;
 
 internal static class VistaExtensions
 {
     internal static Layout? ObtenerLayoutRaizPagina(this Element elemento)
     {
-        Element? actual = elemento;
-        Layout? ultimoLayoutEncontrado = null;
-
-        while (actual is not null)
+        var actual = elemento;
+        while (actual.Parent is not null)
         {
-            if (actual is Layout layoutActual)
-                ultimoLayoutEncontrado = layoutActual;
-
-            if (actual is Popup popup)
-            {
-                var paginaActiva = ObtenerPaginaActiva();
-                if (paginaActiva?.Content is Layout layoutPaginaActiva)
-                    return layoutPaginaActiva;
-
-                if (popup.Content is Layout popupLayout)
-                    return popupLayout;
-
-                if (popup.Content is ContentView contentView && contentView.Content is Layout contentLayout)
-                    return contentLayout;
-
-                return ultimoLayoutEncontrado;
-            }
-
-            if (actual is ContentPage pagina && pagina.Content is Layout layout)
+            if (actual.Parent is ContentPage pagina && pagina.Content is Layout layout)
                 return layout;
-
             actual = actual.Parent;
         }
-
-        return ultimoLayoutEncontrado;
-    }
-
-    private static ContentPage? ObtenerPaginaActiva()
-    {
-        Page? page = Application.Current?.Windows.FirstOrDefault()?.Page;
-
-        if (page is Shell shell)
-            page = shell.CurrentPage;
-
-        if (page is NavigationPage navigationPage)
-            page = navigationPage.CurrentPage;
-
-        if (page is TabbedPage tabbedPage)
-            page = tabbedPage.CurrentPage;
-
-        return page as ContentPage;
+        return null;
     }
 
     internal static ContentPage? ObtenerPagina(this Element elemento)
